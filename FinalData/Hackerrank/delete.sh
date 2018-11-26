73fcb73ad02835b23f72975cb4ee8766e8f356dc
Akshay GOPALAKRISHNAN <aaksha@ds2.CS.McGill.CA>
2018-11-26 12:10:24 -0500

    git comit -m "modified dataset"

--- FinalData/Hackerrank/delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for folder in */; do
-    cd $folder
-    rm stat.txt
-    rm a.out
-    cd ..
-done    
--- FinalData/Hackerrank/get_final_stats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for folder in */; do
-    cd $folder
-    a=$(realpath stat.txt)
-    cd ..
-    echo $folder $(./e_min $a) >> final_stats.txt
-done    
